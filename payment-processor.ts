dd44132d390dea720318e1ab98620fed38576c8e
Tareq Aziz <tareqaziz0065@gmail.com>
2026-03-12 00:44:51 +0600

    Update payment-processor.ts

--- payment-processor.ts
+++ payment-processor.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import fetch from 'node-fetch';
 
-// VULNERABILITY: Hardcoded secret key
-const STRIPE_SECRET_KEY = "sk_live_51MabcdeFghIjklMnOpQrStUvWxYz1234567890abcdef1234567890abcdef";
-
 const prisma = new PrismaClient();
 
 /**
